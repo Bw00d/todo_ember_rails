c711f5b25d2d9e3f42c8d87adac77e5abe19ea71
Brentwood Reid & Harland Hauger <student@epicodus.com>
2014-04-28 17:22:04 -0700

    add task show

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,4 +2,6 @@
 
 TodoApi.Router.map(function() {
   this.resource('tasks', { path: '/' });
+  this.resource('new_task', { path: '/tasks/new' });
+  this.resource('task', { path: '/tasks/:id' });
 });
---
+++ app/assets/javascripts/routes/task_route.js
@@ -0,0 +1,5 @@
+TodoApi.TaskRoute = Ember.Route.extend({
+  model: function(params) {
+    return.this.store.find('task', params.id);
+  }
+});
